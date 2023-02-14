b6263def0d734029ed7e7baa40368a5d0299766d
pavan-kumar-nuthi <pavankumarnuthi@gmail.com>
2023-02-14 14:08:01 +0530

    Jenkins

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout<<"From PES1UG20CS670";
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
